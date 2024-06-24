f86005080d6637449df3d29d25066f4084767f76
Jiangshuuu <globelex65@gmail.com>
2024-06-24 15:20:40 +0800

    add transaction schema

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { handle } from 'hono/vercel'
 
 import accounts from './accounts'
 import categories from './categories'
+import transactions from './transactions'
 
 const app = new Hono().basePath("/api")
 
@@ -17,6 +18,7 @@ const app = new Hono().basePath("/api")
 const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
